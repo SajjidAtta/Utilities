bb154e5c828249d07647595d3050e7ab9796c50b
Sajjid Atta <sajjidattag@gmail.com>
2018-08-02 13:47:52 +0500

    Added Project to Git Repository

---
+++ EmailSender/Emailer.cs
@@ -0,0 +1,129 @@
+﻿using LogWriter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class Emailer
+    {
+        //#TODO: Logging in Setters
+        SmtpClient smtpclient;
+
+        //Receipents , separeted list of emails.
+        String Emails_To, Emails_Cc, Emails_Bcc;
+
+        //Sender Properties
+        String Host, Sender_Email, Sender_Password;
+        int Port;
+
+        MailMessage msg;
+        public Emailer()
+        {
+
+            msg = new MailMessage();
+
+            //Read Data from Config
+            LoadDataFromConfig();
+
+            //Intialize
+            InitializeEmailer();
+        }
+        private void LoadDataFromConfig()
+        {
+            Logger.Instance.LogMethodStart();
+            try
+            {
+
+
+                Emails_To = System.Configuration.ConfigurationManager.AppSettings["Emails_To"];
+                Emails_Cc = System.Configuration.ConfigurationManager.AppSettings["Emails_Cc"];
+                Emails_Bcc = System.Configuration.ConfigurationManager.AppSettings["Emails_Bcc"];
+
+                Host = System.Configuration.ConfigurationManager.AppSettings["Host"];
+                Port = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["Port"]);
+
+                Sender_Email = System.Configuration.ConfigurationManager.AppSettings["Sender_Email"];
+                Sender_Password = System.Configuration.ConfigurationManager.AppSettings["Sender_Password"];
+
+            }
+            catch (Exception exp)
+            {
+                Logger.Instance.Log(Logger.MessageType.ERROR, "Cofig File Reading Error: {0}", exp.Message);
+
+            }
+
+            Logger.Instance.LogMethodEnd();
+        }
+        private void InitializeEmailer()
+        {
+            try
+            {
+                //Create Creditials
+                NetworkCredential Sender_Credintials = new NetworkCredential(Sender_Email, Sender_Password);
+
+                //InitilizeSMTPclient
+                smtpclient = new SmtpClient(Host, Port);
+                smtpclient.EnableSsl = true;
+
+                //Bind Smtp client with Credintials
+                smtpclient.Credentials = Sender_Credintials;
+            }
+            catch (Exception exp)
+            {
+                Logger.Instance.Log(Logger.MessageType.ERROR, "SMTP Intialization Error: {0}", exp.Message);
+
+            }
+
+
+        }
+
+
+
+
+        public void CreateMailMessage(String Subject, String Body, List<Attachment> attachments = null)
+        {
+            try
+            {
+                msg.To.Add(Emails_To);
+                msg.CC.Add(Emails_Cc);
+                msg.Bcc.Add(Emails_Bcc);
+                if (attachments != null)
+                {
+                    //Attach Attachments
+                }
+
+                msg.From = new MailAddress(Sender_Email);
+                msg.Subject = Subject;
+                msg.Body = Body;
+            }
+            catch (Exception exp)
+            {
+                Logger.Instance.Log(Logger.MessageType.ERROR, "Message Creation Error: {0}", exp.Message);
+
+            }
+        }
+
+        public void SendMessage()
+        {
+
+            //Send Message
+            try
+            {
+                smtpclient.Send(msg);
+                Logger.Instance.Log(Logger.MessageType.INF, "Email Delivered: {0}", msg.ToString());
+            }
+            catch (Exception exp)
+            {
+                Logger.Instance.Log(Logger.MessageType.ERROR, "Message Sending Error: {0}", exp.Message);
+
+            }
+
+        }
+
+    }
+}
---
+++ LogWriter/Logger.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogWriter
+{
+    public class Logger
+    {
+        //#TODO:Log Error to windows events
+        
+        public enum MessageType { DEBUG, INF, ERROR };
+        string LogFile;
+        static Logger instance;
+        private Logger()
+        {
+            LogFile = Path.GetFullPath((System.Configuration.ConfigurationManager.AppSettings["LogFilePath"]));
+        }
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Logger();
+                return instance;
+            }
+        }
+        public void Log(MessageType msgtype, string msg, params string[] values)
+        {
+
+            msg = String.Format("{0} -- [{1}] -- {2}", System.DateTime.Now, msgtype.ToString(), String.Format(msg.ToString(), values));
+
+
+            try
+            {
+
+
+                if (!File.Exists(LogFile))
+                {
+
+
+                    using (StreamWriter sw = new StreamWriter(File.Create(LogFile)))
+                    {
+                        sw.WriteLine(msg);
+                        sw.Close();
+                    }
+
+                }
+                else
+                {
+                    using (StreamWriter sw = File.AppendText(LogFile))
+                    {
+                        sw.WriteLine(msg);
+                        sw.Close();
+                    }
+                }
+
+            }
+            catch (Exception exp)
+            {
+                //Write to Event Log
+            }
+
+        }
+        public void LogtoConsole(string msg, params string[] values)
+        {
+            msg = String.Format(msg, values);
+            try
+            {
+
+                Console.WriteLine(System.DateTime.Now + " -- " + msg);
+
+
+
+            }
+            catch (Exception exp)
+            {
+                //Write to Event Log
+            }
+
+        }
+        public void LogMethodStart()
+        {
+            //#TODO: Reflections related Work for Method Names
+            String MethodName = "SomeName";
+            Log(MessageType.DEBUG, "Method Start: {0}", MethodName);
+
+        }
+        public void LogMethodEnd()
+        {
+            //#TODO: Reflections related Work for Method Names
+            String MethodName = "SomeName";
+            Log(MessageType.DEBUG, "Method End: {0}", MethodName);
+        }
+    }
+}
